a8eaf48a9df4288cfcef6979d68df55a3ee5e9d6
Antonio Cuadros Lapresta <antculap@gmail.com>
2022-06-27 12:50:59 +0200

    Closes #17, added method to get a new portfolio

--- test/PortfolioManagerTest.rb
+++ test/PortfolioManagerTest.rb
@@ -8,13 +8,29 @@ class PortfolioManagerTest < Minitest::Test
     def setup
         @portfolioManager = PortfolioManager.new()
         @portfolio = Portfolio.new("onePortfolio")
+        @portfolio2 = Portfolio.new("anotherPortfolio")
     end
 
-    def test_addPortfolio
+    def test_AddPortfolio
         @portfolioManager.AddPortfolio(@portfolio)
 
         assert_equal 1, @portfolioManager.GetNumPortfolios
         assert_equal "onePortfolio", @portfolioManager.GetPortfolio(@portfolio).name
     end
 
+    def test_GetPortfolio_PortfolioExists_PortfolioReturned
+        @portfolioManager.AddPortfolio(@portfolio)
+
+        assert_equal 1, @portfolioManager.GetNumPortfolios
+        assert_equal "onePortfolio", @portfolioManager.GetPortfolio(@portfolio).name
+    end
+
+    def test_GetPortfolio_PortfolioDoesNotExist_NilReturned
+        @portfolioManager.AddPortfolio(@portfolio)
+
+        assert_equal 1, @portfolioManager.GetNumPortfolios
+        assert_nil @portfolioManager.GetPortfolio(@portfolio2)
+    end
+
+    
 end
